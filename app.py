8a0fabffa941cd564695b9092e67de1e3bb2030f
Raviteja-5976 <120631044+Raviteja-5976@users.noreply.github.com>
2023-07-06 12:20:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,49 @@
+import pyttsx3
 import streamlit as st
-from text_to_speech import read_text , stop_speech
+
+engine = None
+is_speaking = False  # Global variable to track the speech state
+
+def preprocess_text(text, speed):
+    # Preprocessing logic
+    words = text.split()
+    processed_text = []
+    for word in words:
+        if len(word) > 5:
+            processed_text.append(f"{word} , {' , '.join(list(word))} \t {word}")
+        else:
+            processed_text.append(word)
+    processed_text = '\n'.join(processed_text)
+    processed_text = processed_text.replace('\n\n', '\n\nnext paragraph\n\n')
+    return processed_text
+
+def read_text(text, speed):
+    # Read text logic
+    global engine, is_speaking
+
+    if engine is None:
+        engine = pyttsx3.init()
+
+    if not is_speaking:
+        # Preprocess the text
+        processed_text = preprocess_text(text, speed)
+
+        # Speak the text
+        engine.setProperty('rate', speed)
+        engine.say(processed_text)
+        engine.runAndWait()
+
+        is_speaking = True
+
+def stop_speech():
+    # Stop speech logic
+    global engine, is_speaking
+
+    if engine is not None and is_speaking:
+        engine.stop()
+        is_speaking = False
+
+
 
 st.title('WRITE WITH VOICE')
 st.header('by Raviteja')
@@ -9,10 +53,15 @@ st.caption('RECOMMENDED SPEED 120-180')
 
 user_input = st.text_area('Input')
 
+if (user_input==None) :
+    st.caption('please fill the input space')
+
 user_speed = st.number_input('Speed')
 
+if (user_speed==None):
+    st.caption('please enter the speed')
+
 if user_input and user_speed:
-    if st.button('Speak'):
+    but=st.button('Speak')
+    if but:
         read_text(user_input, user_speed) 
-    elif st.button('Stop'):
-        stop_speech()
